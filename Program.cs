10997e47b7a9ba29370fd8fce7dc9ff1fc01d985
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-20 21:33:54 -0300

    testando

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Swashbuckle.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Apirest_teste.db;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbLivrariaContext>(opt =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("livrariaConnection");
+    var serverVersion = ServerVersion.AutoDetect(connectionString);
+    opt.UseMySql(connectionString, serverVersion);
+});
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.MapGet("/api/livros", ([FromServices] DbLivrariaContext _db) => 
+{
+    return Results.Ok(_db.TbLivro.ToList<TbLivro>());
+});
+
+app.Run();
